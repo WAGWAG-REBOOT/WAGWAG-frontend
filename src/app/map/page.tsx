d89f44ddbf6c2cb316680fa97bc43d46212f3c08
rhdmswls12 <rhgusdnr1293@naver.com>
2025-07-13 18:04:04 +0900

    feat: GeoJSON 기반 폴리곤 클릭 이벤트 임시 구현

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -5,7 +5,7 @@ import { useRef } from "react";
 
 export default function MapPage() {
   const mapRef = useRef<naver.maps.Map | null>(null);
-  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const polygonRef = useRef<naver.maps.Polygon | null>(null);
 
   const initializeMap = () => {
     const map = new naver.maps.Map("map", {
@@ -19,9 +19,9 @@ export default function MapPage() {
       .then((res) => res.json())
       .then((geojson) => {
         geojson.features.forEach((feature: any) => {
-          const { coordinates, type } = feature.geometry;
+          const { type, coordinates } = feature.geometry;
 
-          const paths: naver.maps.LatLng[][] = [];
+          let paths: naver.maps.LatLng[][] = [];
 
           if (type === "Polygon") {
             paths.push(
@@ -31,16 +31,37 @@ export default function MapPage() {
             coordinates.forEach((polygon: number[][][]) => {
               paths.push(polygon[0].map(([lng, lat]: number[]) => new naver.maps.LatLng(lat, lng)));
             });
+          } else {
+            return;
           }
 
-          new naver.maps.Polygon({
-            map,
+          const dummyPolygon = new naver.maps.Polygon({
             paths,
-            strokeColor: "#FF0000",
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#FF0000",
-            fillOpacity: 0.3,
+            map,
+            strokeOpacity: 0,
+            fillOpacity: 0,
+            clickable: true,
+            fillColor: "transparent",
+          });
+
+          // 클릭 시 실제 폴리곤 생성
+          naver.maps.Event.addListener(dummyPolygon, "click", () => {
+            // 기존 폴리곤이 있다면 제거
+            if (polygonRef.current) {
+              polygonRef.current.setMap(null);
+            }
+
+            const polygon = new naver.maps.Polygon({
+              map,
+              paths,
+              strokeColor: "#57F98E",
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: "#57F98E",
+              fillOpacity: 0.4,
+            });
+
+            polygonRef.current = polygon;
           });
         });
       });
@@ -54,7 +75,6 @@ export default function MapPage() {
         onReady={initializeMap}
       />
       <div
-        ref={mapContainerRef}
         id="map"
         style={{ width: "100%", height: "100vh" }}
       />
