c582975725d3fa77d0637c6f289487a159a2aab1
seungboshim-iconx <sb.shim@iconx.co.kr>
2025-10-12 16:21:22 +0900

    refactor: [WAGGY-24] 레이아웃 수정 - route group 활용하여 페이지 레이아웃 수정

--- src/app/map/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function MapLayout({ children }: { children: React.ReactNode }) {
-  return <div className="ml-[150px]">{children}</div>;
-}
--- src/app/upload/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function UploadLayout({ children }: { children: React.ReactNode }) {
-  return <div className="ml-[150px]">{children}</div>;
-}
